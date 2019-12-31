a9affc0f98f124509a0cd08d19ae982ae411c585
Lucas Miller <lucasmiller@Lucass-MBP.attlocal.net>
2019-12-31 16:22:07 -0600

    update profile

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ app.route("/")
 
 
 
-    const APIkey = "47ec556034454d8caf815808190412";
+    const APIkey = " 47ec556034454d8caf815808190412";
     const ZIP = req.body.zipcode;
     const url = "http://api.weatherapi.com/v1/forecast.json?key="+ APIkey+"&q=" + city + "&days=3";
     request(url, function(error, response, body) {
