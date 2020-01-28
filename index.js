4f93b846414a6bd84b35d52387639dc17c97e7c2
Lucas Miller <lucasmiller@Lucass-MBP.attlocal.net>
2020-01-27 21:53:33 -0600

    update api key

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const app = express();
-const request = require('request');
+const request = require("request");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.set("view engine", "ejs");
 
-app.set(express.static('public'));
+app.set(express.static("public"));
 
 //routes
-app.route("/")
+app
+  .route("/")
   .get(function(req, res) {
-
     var date = new Date();
     var day = date.getDay();
-
 
     var weekday;
     switch (day) {
@@ -42,49 +43,47 @@ app.route("/")
         weekday = "Saturday";
         break;
       default:
-
     }
 
     switch (date.getMonth()) {
       case 0:
-        var month = "JAN"
+        var month = "JAN";
         break;
       case 1:
-        var month = "FEB"
+        var month = "FEB";
         break;
       case 2:
-        var month = "MAR"
+        var month = "MAR";
         break;
       case 3:
-        var month = "APR"
+        var month = "APR";
         break;
       case 4:
-        var month = "MAY"
+        var month = "MAY";
         break;
       case 5:
-        var month = "JUN"
+        var month = "JUN";
         break;
       case 6:
-        var month = "JUL"
+        var month = "JUL";
         break;
       case 7:
-        var month = "AUG"
+        var month = "AUG";
         break;
       case 8:
-        var month = "SEP"
+        var month = "SEP";
         break;
       case 9:
-        var month = "OCT"
+        var month = "OCT";
         break;
       case 10:
-        var month = "NOV"
+        var month = "NOV";
         break;
       case 11:
-        var month = "DEC"
+        var month = "DEC";
         break;
 
       default:
-
     }
 
     res.render("index", {
@@ -99,15 +98,15 @@ app.route("/")
       day1: null,
       day2: null,
       day3: null,
-      day1Weather:0,
-      day2Weather:0,
-      day3Weather:0,
+      day1Weather: 0,
+      day2Weather: 0,
+      day3Weather: 0,
       icon1: null,
       icon2: null,
-      icon3: null,
+      icon3: null
     });
-  }).post(function(req, res) {
-
+  })
+  .post(function(req, res) {
     var date = new Date();
     var day = date.getDay();
     var weekday;
@@ -133,7 +132,6 @@ app.route("/")
         weekday = "Saturday";
         break;
       default:
-
     }
     switch (day) {
       case 0:
@@ -157,9 +155,8 @@ app.route("/")
         smallDay = "SAT";
         break;
       default:
-
     }
-    switch (day+1) {
+    switch (day + 1) {
       case 0:
         smallDay2 = "SUN";
         break;
@@ -181,9 +178,8 @@ app.route("/")
         smallDay2 = "SAT";
         break;
       default:
-
     }
-    switch (day+2) {
+    switch (day + 2) {
       case 0:
         smallDay3 = "SUN";
         break;
@@ -205,62 +201,63 @@ app.route("/")
         smallDay3 = "SAT";
         break;
       default:
-
     }
     switch (date.getMonth()) {
       case 0:
-        var month = "JAN"
+        var month = "JAN";
         break;
       case 1:
-        var month = "FEB"
+        var month = "FEB";
         break;
       case 2:
-        var month = "MAR"
+        var month = "MAR";
         break;
       case 3:
-        var month = "APR"
+        var month = "APR";
         break;
       case 4:
-        var month = "MAY"
+        var month = "MAY";
         break;
       case 5:
-        var month = "JUN"
+        var month = "JUN";
         break;
       case 6:
-        var month = "JUL"
+        var month = "JUL";
         break;
       case 7:
-        var month = "AUG"
+        var month = "AUG";
         break;
       case 8:
-        var month = "SEP"
+        var month = "SEP";
         break;
       case 9:
-        var month = "OCT"
+        var month = "OCT";
         break;
       case 10:
-        var month = "NOV"
+        var month = "NOV";
         break;
       case 11:
-        var month = "DEC"
+        var month = "DEC";
         break;
 
       default:
-
     }
     const city = req.body.city;
 
-
-
-    const APIkey = " 47ec556034454d8caf815808190412";
+    const APIkey = " 3319c5ff8a6b41ac830191943200501";
     const ZIP = req.body.zipcode;
-    const url = "http://api.weatherapi.com/v1/forecast.json?key="+ APIkey+"&q=" + city + "&days=3";
+    const url =
+      "http://api.weatherapi.com/v1/forecast.json?key=" +
+      APIkey +
+      "&q=" +
+      city +
+      "&days=3";
     request(url, function(error, response, body) {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log("error:", error); // Print the error if one occurred
+      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
       // console.log('body:', body); // Print the HTML for the Google homepage.
-      const apiResponse = JSON.parse(response.body)
-      const apiResponse2 =  JSON.parse(response.body)
+      const apiResponse = JSON.parse(response.body);
+      const apiResponse2 = JSON.parse(response.body);
       if (response && response.statusCode == 200) {
         res.render("index", {
           date: date.getDate() + " " + month + " " + date.getFullYear(),
@@ -274,14 +271,12 @@ app.route("/")
           day1: smallDay,
           day2: smallDay2,
           day3: smallDay3,
-          day1Weather:apiResponse2.forecast.forecastday[0].day.avgtemp_f,
-          day2Weather:apiResponse2.forecast.forecastday[1].day.avgtemp_f,
-          day3Weather:apiResponse2.forecast.forecastday[2].day.avgtemp_f,
+          day1Weather: apiResponse2.forecast.forecastday[0].day.avgtemp_f,
+          day2Weather: apiResponse2.forecast.forecastday[1].day.avgtemp_f,
+          day3Weather: apiResponse2.forecast.forecastday[2].day.avgtemp_f,
           icon1: apiResponse2.forecast.forecastday[0].day.condition.icon,
           icon2: apiResponse2.forecast.forecastday[1].day.condition.icon,
-          icon3: apiResponse2.forecast.forecastday[2].day.condition.icon,
-
-
+          icon3: apiResponse2.forecast.forecastday[2].day.condition.icon
         });
       } else {
         res.render("index", {
@@ -296,30 +291,18 @@ app.route("/")
           day1: null,
           day2: null,
           day3: null,
-          day1Weather:0,
-          day2Weather:0,
-          day3Weather:0,
+          day1Weather: 0,
+          day2Weather: 0,
+          day3Weather: 0,
           icon1: null,
           icon2: null,
-          icon3: null,
+          icon3: null
         });
       }
-
-
     });
-
-
-
-  })
-
-
-
-
-
-
-
+  });
 
 //listening
 app.listen(process.env.PORT || 3000, function() {
   console.log("server is running on port 3000");
-})
+});
